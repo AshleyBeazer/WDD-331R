e780219f328f68d2ece96c6f914ad72da46b1dbc
AshleyBeazer <ashley.mckeehan96@gmail.com>
2022-03-11 14:15:22 -0700

    Updated the weather

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,9 +1,31 @@
-let t = parseFloat(document.querySelector(".temperature").innerHTML);
-let s = parseFloat(document.querySelector(".wind-speed").innerHTML);
-let windChill = (35.74 + (0.6215 * t)) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16));
-if (t <= 50.0 && s > 3.0) {
-    document.querySelector("windChill").innerHTML = Math.ceil(windChill)
-}
-else {
-    document.querySelector("windChill").innerHTML = "N/A"
-}
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5782391&units=imperial&appid=0abbbbf1b3af22801480d87a6b128ad3";
+fetch(apiURL)
+    .then ((response) => response.json())
+    .then((jsObject) => {
+
+        let t = parseFloat(jsObject.main.temp);
+    let s = parseFloat(jsObject.wind.speed);
+    let output = "N/A";
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+
+    document.getElementById("weather").innerHTML =
+      jsObject.weather[0].description;
+    document.getElementById("current-temp").innerHTML =
+      Math.round(t) + "&#8457;";
+    if (t <= 50 && s >= 3) {
+      let f =
+        35.74 +
+        0.6215 * t -
+        35.75 * Math.pow(s, 0.16) +
+        0.4275 * (t * Math.pow(s, 0.16));
+      output = Math.round(f);
+    }
+    document.getElementById("output").innerHTML = output + "&#8457;";
+    document.getElementById("speed").innerHTML = Math.round(s) + " mph";
+
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+});
---
+++ lesson10/api.js
@@ -0,0 +1,14 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=0abbbbf1b3af22801480d87a6b128ad3";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+    console.table(jsonObject);
+    let temp = document.querySelector('#current-temp');
+    temp.textContent = jsonObject.main.temp.toFixed(1);
+    const iconsrc= `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`;
+    const desc = jsonObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+});
