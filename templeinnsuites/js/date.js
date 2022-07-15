02a0fc8cfe5e3b738f525c832836d1acf28a9c4d
AshleyBeazer <97131595+AshleyBeazer@users.noreply.github.com>
2022-07-15 16:42:47 -0600

    Delete templeinnsuites directory

--- templeinnsuites/js/date.js
+++
@@ -1,8 +0,0 @@
-let lastModified = document.lastModified;
-    
-    document.getElementById('lastUpdate').textContent = (` Updated: ${lastModified}`);
-    let currentYear = document.querySelector('#currentYear');
-
-    year = new Date().getFullYear();
-
-    currentYear.textContent = year;
--- templeinnsuites/js/navigation.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    function toggleMenu() {
-        document.getElementById('Nav').classList.toggle('open');
-        document.getElementById('hamburgerMenu').classList.toggle('open');
-    }
-    const x = document.getElementById('hamburgerMenu');
-    x.onclick = toggleMenu;
-}
